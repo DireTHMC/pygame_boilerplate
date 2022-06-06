7cd874f965de08df8591ca73290bc003e0786720
группы стационара <st@itstep.lan>
2022-06-06 21:44:56 +0300

    GameV2

--- MyNewGameV2.py
+++ MyNewGameV2.py
@@ -1,13 +1,11 @@
 import pygame
 import sys
 from pygame.color import THECOLORS
-import time
-import random
 
 pygame.init()
 
 # Создание экрана
-display_width = 400
+display_width = 1200
 display_height = 800
 screen = pygame.display.set_mode((display_width, display_height))
 
@@ -17,7 +15,6 @@ pygame.display.set_caption("Don't crush my car, dude!")
 #colors
 black = (0, 0, 0)
 white = (255, 255, 255)
-red = (255, 0, 0)
 
 #Модуль для времени, чтобы мониторить кадры в секунду
 clock = pygame.time.Clock()
@@ -25,48 +22,16 @@ clock = pygame.time.Clock()
 #Создание героя
 carimg = pygame.image.load("Image/car_aston.png")
 carimg = pygame.transform.scale(carimg, (70, 80))
-car_width = 70
-
-#Функция для препятствий
-def things(thing_x, thing_y, thing_w, thing_h, color):
-    pygame.draw.rect(screen, color, [thing_x, thing_y, thing_w, thing_h])
 
 #Отрисовка авто
 def car(x, y):
     screen.blit(carimg, (x, y))
 
-#Вывод текста
-def text_objects(text , font):
-    textsurface = font.render(text, True, black)
-    return textsurface, textsurface.get_rect()
-
-def message_display(text):
-    largetext = pygame.font.Font('freesansbold.ttf', 20)
-    TextSurf, TextRect = text_objects(text, largetext)
-    TextRect.center = ((display_width/2), (display_height/2))
-    screen.blit(TextSurf, TextRect)
-
-    pygame.display.update()
-
-    time.sleep(5)
-
-    game_loop()
-
-def crash():
-    message_display('You crashed, looser!')
-
-
 #Запуск игры
 def game_loop():
     # Размещение авто
-    x = (165)
+    x = (565)
     y = (710)
-
-    thing_start_x = random.randrange(0, display_width)
-    thing_start_y = -800
-    thing_speed = 5
-    thing_width = 70
-    thing_height = 80
 
     # условия игры
     x_change = 0  # смещение
@@ -98,18 +63,8 @@ def game_loop():
         x += x_change
         #Создание фона
         screen.fill(THECOLORS["bisque3"])
-        things(thing_start_x, thing_start_y, thing_width, thing_height, black)
-        thing_start_y += thing_speed
-
         #Появление машинки
         car(x, y)
-        if x > display_width - car_width or x < 0:
-            gameexit = True
-            crash()
-        if thing_start_y > display_height:
-            thing_start_y = 0 - thing_height
-            thing_start_x = random.randrange(0, display_width)
-
         #Проверка дисплея на обновление
         pygame.display.update()
         #Кадры в секунду
@@ -118,4 +73,3 @@ def game_loop():
 game_loop()
 pygame.quit()
 quit()
-
